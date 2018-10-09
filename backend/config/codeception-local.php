3c784df6cf78eabf630318b56d8f085c96b1e1df
SaloEater <danil.lazarev@gmail.com>
2018-10-09 20:37:18 +0300

    Last fixes for codeception

---
+++ backend/config/codeception-local.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom
+ * Date: 09.10.2018
+ * Time: 18:48
+ */
+return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/test-local.php'),
+    require(__DIR__ . '/main.php'),
+    require(__DIR__ . '/main-local.php'),
+    require(__DIR__ . '/test.php'),
+    [
+    ]
+);
